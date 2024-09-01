8548c3bef6e665eff45286cfcab3c943417e838c
rithani <rithaniravi@gmail.com>
2024-09-01 19:38:44 +0530

    change

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,19 @@
 import './footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook,faInstagram,faLinkedin} from '@fortawesome/free-brands-svg-icons'
+import { useState} from "react";
 
 const Footer=()=>{
     const handleClick=(e)=>{
-        window.alert("Subscribed Succefully")
+        
         e.preventDefault()
+        alert("Thank you for your subscription")
+        setDisable(1)
+        
+
     }
+    const[Mail,setMail]=useState("")
+    const [disable,setDisable]=useState(0)
     return(
         <div className='container-fluid-expand-sm'>
             <footer className="footerp-2 rounded bg-secondary">
@@ -14,15 +21,19 @@ const Footer=()=>{
                 <div class="row p-1">
                     <div class="col-sm-6 text-center p-3">
                         <h5 className='text-white font-weight-bold'>SOCIAL MEDIA</h5>
-                        <FontAwesomeIcon icon={faFacebook} style={{color: "#cfcfcf",height:"20",width:"30"}} />
-                        <FontAwesomeIcon icon={faInstagram} style={{color: "#cfcfcf",height:"20",width:"30"}} />
-                        <FontAwesomeIcon icon={faLinkedin} style={{color: "#cfcfcf",height:"20",width:"30"}} />
+                        <button className='btn'><FontAwesomeIcon icon={faFacebook} style={{color: "#cfcfcf",height:"20",width:"30"}} /></button>
+                        <button className='btn'><FontAwesomeIcon icon={faInstagram} style={{color: "#cfcfcf",height:"20",width:"30"}} /></button>
+                        <button className='btn'><FontAwesomeIcon icon={faLinkedin} style={{color: "#cfcfcf",height:"20",width:"30"}} /></button>
                     </div>
                     <div class="col-sm-6 text-center P-3">
                         <h5 className='text-white'>STAY UPDATE WITH US</h5>
-                        <input type='text' placeholder='Enter Your Email Id' onChange ={(event)=>}  
-                        className='input rounded p-2 m-2'></input>
-                        <button className='btn btn-danger m-2 mt-1' onClick={handleClick}>Subscribe</button>
+                        <input type='text' placeholder='Enter Your Email Id' 
+                        ref={({required:"mailId required"})}
+                        
+                        value={Mail} 
+                        onChange={(e)=>setMail(e.target.value)} 
+                        className='input rounded p-2 m-2'/>
+                        <button className='btn btn-danger m-2 mt-1' disable={disable} onClick={handleClick}>Subscribe</button>
 
                     </div>
                 </div>
